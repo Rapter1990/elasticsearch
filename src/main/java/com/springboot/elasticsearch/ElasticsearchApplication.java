fe891845fed46050dda521906aba87fbd2e979b8
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-05 15:41:48 +0300

    Task 6 : Enable ElasticSearchRepository in ElasticsearchApplication

--- src/main/java/com/springboot/elasticsearch/ElasticsearchApplication.java
+++ src/main/java/com/springboot/elasticsearch/ElasticsearchApplication.java
@@ -2,8 +2,10 @@ package com.springboot.elasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableElasticsearchRepositories
 public class ElasticsearchApplication {
 
     public static void main(String[] args) {

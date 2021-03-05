79ae1a28178aa13f8418a723755bb20aaf04c1b1
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-05 16:00:18 +0300

    Task 8 : Add ElasticSearch Query methods in MovieService

--- src/main/java/com/springboot/elasticsearch/service/MovieService.java
+++ src/main/java/com/springboot/elasticsearch/service/MovieService.java
@@ -3,12 +3,20 @@ package com.springboot.elasticsearch.service;
 import com.springboot.elasticsearch.model.Director;
 import com.springboot.elasticsearch.model.Movie;
 import com.springboot.elasticsearch.repository.MovieRepository;
+import com.springboot.elasticsearch.util.Constants;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.elasticsearch.index.query.Operator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
 
 
 @Service
@@ -56,6 +64,40 @@ public class MovieService {
             throw new RuntimeException("No Record With this Id!");
                 }
         );
+    }
+
+    public Page<Movie> searchMovieName(String search) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(matchQuery(Constants.MOVIE_NAME, search))
+                .build();
+        return movieRepository.search(searchQuery);
+    }
+
+    public Page<Movie> searchMovieWithFuzzines(String search) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(matchQuery(Constants.MOVIE_NAME, search)
+                        .operator(Operator.AND)
+                        .fuzziness(Fuzziness.AUTO)
+                        .prefixLength(3)) // not changed in first 3 letters of search text
+                .build();
+        return movieRepository.search(searchQuery);
+    }
+
+    public Page<Movie> searchMovieWithMultiMatch(String search) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(multiMatchQuery(search)
+                        .field(Constants.MOVIE_NAME)
+                        .field(Constants.MOVIE_DIRECTOR)
+                        .type(MultiMatchQueryBuilder.Type.BEST_FIELDS))
+                .build();
+        return movieRepository.search(searchQuery);
+    }
+
+    public Page<Movie> searchMovieWithFullText(String search) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(matchPhraseQuery(Constants.MOVIE_NAME, search).slop(1))
+                .build();
+        return movieRepository.search(searchQuery);
     }
 
 }

c141bbed80c68941f5ab708ba2ed5f0ca7c89ffd
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-05 16:17:36 +0300

    Task 11 : Define MovieController

--- src/main/java/com/springboot/elasticsearch/controller/MovieController.java
+++ src/main/java/com/springboot/elasticsearch/controller/MovieController.java
@@ -1,16 +1,92 @@
 package com.springboot.elasticsearch.controller;
 
+import com.springboot.elasticsearch.model.Director;
+import com.springboot.elasticsearch.model.Movie;
 import com.springboot.elasticsearch.service.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/movies")
 public class MovieController {
 
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
     @Autowired
     MovieService movieService;
 
+    @PostMapping("/save")
+    public Movie saveBook(@RequestBody Movie movie) {
+        return movieService.saveMovie(movie);
+    }
 
+    @GetMapping("/find/all")
+    public Page<Movie> allBooks(@PageableDefault(sort = {"id"}, size = 25, page = 0) Pageable pageable) {
+        return movieService.allMovies(pageable);
+    }
+
+    @GetMapping("/{search}")
+    public Page<Movie> searchMovie(@PathVariable String search) {
+        return movieService.searchMovieName(search);
+    }
+
+    @GetMapping("/fuzzines/{search}")
+    public Page<Movie> searchMovieWithFuzzines(@PathVariable String search) {
+        return movieService.searchMovieWithFuzzines(search);
+    }
+
+    @GetMapping("/multi/{search}")
+    public Page<Movie> searchMovieWithMultiMatch(@PathVariable String search) {
+        return movieService.searchMovieWithMultiMatch(search);
+    }
+
+    @GetMapping("/full_text_search/{search}")
+    public Page<Movie> searchMovieWithFullText(@PathVariable String search) {
+        return movieService.searchMovieWithFullText(search);
+    }
+
+    @GetMapping("/find/{id}")
+    public Optional<Movie> findById(@PathVariable Long id) {
+        LOG.info("Getting find movie By id, ", id);
+        return movieService.findById(id);
+    }
+
+    @GetMapping("/find_name/{name}")
+    public List<Movie> findByName(@PathVariable String name) {
+        LOG.info("Getting find movie By name, ", name);
+        return movieService.getByName(name);
+    }
+
+    @GetMapping("/getByRatingInterval/start/{start}/end/{end}")
+    public List<Movie> getByRatingInterval(@PathVariable Double start, @PathVariable Double end) {
+        LOG.info("getByRatingInterval start : " + start + " , end : " + end);
+        return movieService.getByRatingInterval(start,end);
+    }
+
+    @GetMapping("/find_director")
+    public List<Movie> findByDirector(@RequestBody Director director) {
+        LOG.info("findByDirector, ", director.toString());
+        return movieService.findByDirector(director);
+    }
+
+    @PutMapping("/update/{id}")
+    public Movie updateMovie(@RequestBody Movie movie,@PathVariable Long id) {
+        LOG.info("Updating Movie with id {}", id);
+        movieService.update(id,movie);
+        return movie;
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void deleteEmployeeByID(@PathVariable Long id) {
+        LOG.info("Deleting Movie with id {}", id);
+        movieService.deleteMovie(id);
+    }
 }

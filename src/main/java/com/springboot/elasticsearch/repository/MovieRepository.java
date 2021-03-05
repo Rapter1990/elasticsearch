85953bf5c7c4bc8ded8b67dff2e262100f57cf65
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-05 15:14:27 +0300

    Task 4 : Add MovieRepository covering ElasticSearchRepository

---
+++ src/main/java/com/springboot/elasticsearch/repository/MovieRepository.java
@@ -0,0 +1,16 @@
+package com.springboot.elasticsearch.repository;
+
+import com.springboot.elasticsearch.model.Director;
+import com.springboot.elasticsearch.model.Movie;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MovieRepository extends ElasticsearchRepository<Movie, Long> {
+
+    List<Movie> findByName(String name);
+    List<Movie> findByRatingBetween(Double start, Double end);
+    List<Movie> findByDirector(Director director);
+}
